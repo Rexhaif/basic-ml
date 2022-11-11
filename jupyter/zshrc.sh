dffd0c45c2f09d1674b1d68fc888348004d67230
Daniil Larionov <rexhaif.io@gmail.com>
2022-11-11 04:17:59 +0300

    fix zsh completions

---
+++ jupyter/zshrc.sh
@@ -0,0 +1,8 @@
+autoload -U compinit promptinit
+
+promptinit
+prompt pure
+
+compinit
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+fpath=(/usr/local/share/zsh-completions $fpath)
